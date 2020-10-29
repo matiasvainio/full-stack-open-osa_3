368485c66f4271ce24248c10f2f089c3e9764284
matiasva <matias.vainio@metropolia.fi>
2020-10-29 10:33:50 +0200

    phelinluettelon back end step1 done

---
+++ puhelinluettelon_back_end/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+
+const persons = [
+    {
+        name: "Arto Hellas",
+        number: "050-123456789",
+        id: 1,
+    },
+    {
+        name: "Ada Lovelace",
+        number: "39-44-5323523",
+        id: 2,
+    },
+    {
+        id: 3,
+        name: "Dan Abramov",
+        number: "12-43-234345",
+    },
+    {
+        name: "Mary Poppendieck",
+        number: "39-23-6423122",
+        id: 4,
+    },
+];
+
+app.get("/api/persons", (require, response) => {
+    response.json(persons);
+});
+
+const port = 3001;
+app.listen(port);
+console.log(`server running on port ${port}`);
